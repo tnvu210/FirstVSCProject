5a18be7031414f3a3c8feccb40f9c505cef070fe
Thanh Vu <thanh.vu@infosys.com>
2020-05-21 12:45:36 -0500

    Age changed

--- force-app/main/default/lwc/myFirstWebComponent/myFirstWebComponent.js
+++ force-app/main/default/lwc/myFirstWebComponent/myFirstWebComponent.js
@@ -6,26 +6,31 @@ export default class HelloIteration extends LightningElement {
             Id: 1,
             Name: 'Amy Taylor',
             Title: 'VP of Engineering',
+            Age: 45,
         },
         {
             Id: 2,
             Name: 'Michael Jones',
             Title: 'VP of Sales',
+            Age: 50,
         },
         {
             Id: 3,
             Name: 'Jennifer Wu',
             Title: 'CEO',
+            Age: 40,
         },
         {
             Id: 4,
             Name: 'Thanh Vu',
             Title: 'CEO',
+            Age: 33,
         },
         {
             Id: 5,
             Name: 'Aaron Vu',
             Title: 'CEO Child',
+            Age: 0,
         },
     ];
 }
