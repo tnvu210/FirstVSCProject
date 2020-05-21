6f5c9ab0a93d92df5b0a1e0e1ad5951fe4b1420a
Thanh Vu <thanh.vu@infosys.com>
2020-05-21 12:04:49 -0500

    first commit

---
+++ force-app/main/default/lwc/myFirstWebComponent/myFirstWebComponent.js
@@ -0,0 +1,26 @@
+import { LightningElement, track } from 'lwc';
+export default class HelloIteration extends LightningElement {
+    @track
+    contacts = [
+        {
+            Id: 1,
+            Name: 'Amy Taylor',
+            Title: 'VP of Engineering',
+        },
+        {
+            Id: 2,
+            Name: 'Michael Jones',
+            Title: 'VP of Sales',
+        },
+        {
+            Id: 3,
+            Name: 'Jennifer Wu',
+            Title: 'CEO',
+        },
+        {
+            Id: 4,
+            Name: 'Thanh Vu',
+            Title: 'CEO',
+        },
+    ];
+}
